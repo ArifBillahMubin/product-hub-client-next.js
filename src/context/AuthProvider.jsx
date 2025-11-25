a3bad85d4236632ef086d358ff769ec63fff1c53
arif <arifbillahmubin@gmail.com>
2025-11-25 18:54:42 +0600

    firebase setup

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+import { auth } from "@/firebase/firebase.config";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut
+} from "firebase/auth";
+
+const googleProvider = new GoogleAuthProvider();
+
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Register
+    const registerUser = (email, password) =>
+        createUserWithEmailAndPassword(auth, email, password);
+
+    // Login
+    const signInUser = (email, password) =>
+        signInWithEmailAndPassword(auth, email, password);
+
+    // Google Login
+    const signInGoogle = () => signInWithPopup(auth, googleProvider);
+
+    // Logout
+    const signOutUser = () => signOut(auth);
+
+    // Track user login state
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        registerUser,
+        signInUser,
+        signInGoogle,
+        signOutUser,
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
